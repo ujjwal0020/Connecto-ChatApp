69ae7841b8060246a061af5e5846a7cacecff304
ujjwal0020 <113961256+ujjwal0020@users.noreply.github.com>
2025-05-28 04:29:35 +0530

    api updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-export const axiosInstance=axios.create({
-    baseURL: import.meta.env.MODE==="developement" ?"http://localhost:5001/api": "https://connecto-chatapp-1.onrender.com",
-    withCredentials:true,
-    
-})
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://connecto-chatapp-1.onrender.com/api", // ✅ /api lagana zaroori hai
+  withCredentials: true,
+});
