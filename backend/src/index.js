084905fe312d9fc654818780c9fb7b97ac8721dc
ujjwal0020 <113961256+ujjwal0020@users.noreply.github.com>
2025-05-28 04:51:41 +0530

    updated api

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://connecto-chatapp.onrender.com",
+    origin: "https://connecto-chatapp-2.onrender.com",
     credentials: true,
   })
 );
